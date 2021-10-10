f778efe4fac3b12cc71120dcaf19e35b8347fcf4
hoangchung16-00 <hoangchung1687@gmail.com>
2021-10-10 21:08:24 +0700

    init project

---
+++ src/main/java/com/example/jobboard_finalproject/JobboardFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobboard_finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardFinalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobboard_finalproject/JobboardFinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobboard_finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardFinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
